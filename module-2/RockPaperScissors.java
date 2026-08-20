0d479648ae7cf4f1e0aaec34bc22882ed9cf6b98
Eric <erxnoi@gmail.com>
2026-08-20 15:22:42 -0500

    push update to mod 2 java file

--- module-2/RockPaperScissors.java
+++ module-2/RockPaperScissors.java
@@ -1,10 +1,18 @@
 
 
-import java.util.Scanner;
+import java.util.Scanner; // for input
 
 public class RockPaperScissors {
     public static void main(String[] args) {
         
+    // Generate random integer 
+    int randomNumber = (int) (Math.random() * 3) + 1; 
+    
+    // link number to rock, paper, or scissors
+    if (randomNumber == 1) {
+        System.out.println("Computer chose: Rock");
+    }
+
     }    
 
 }
