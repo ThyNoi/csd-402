0b550f1538174137a6d8da52e16d2f8aa9ed2616
Eric <erxnoi@gmail.com>
2026-08-21 15:06:25 -0500

    push draft of mod 3 java file

---
+++ module-3/Pyramid.java
@@ -0,0 +1,20 @@
+/*
+    Eric Sengvanhpheng
+    August 21, 2026
+    CSD 402 Module 3.2
+
+*/
+
+public class Pyramid {
+    public static void main(String[] args) {
+        
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j<= i; j++) {
+            System.out.println("*");
+        }
+      System.out.println(i);
+    }
+
+
+    }
+}   
