c52d64d92287183ee7bf4a29032798c34803f076
ThyNoi <erxnoi@gmail.com>
2026-08-20 19:53:21 -0500

    Update RockPaperScissors.java

--- module-2/RockPaperScissors.java
+++ module-2/RockPaperScissors.java
@@ -1,18 +1,33 @@
 
-
 import java.util.Scanner; // for input
 
 public class RockPaperScissors {
     public static void main(String[] args) {
+
+        int ROCK = 1;
+        int PAPER = 2;
+        int SCISSORS = 3;
         
-    // Generate random integer 
-    int randomNumber = (int) (Math.random() * 3) + 1; 
-    
-    // link number to rock, paper, or scissors
-    if (randomNumber == 1) {
-        System.out.println("Computer chose: Rock");
+        Scanner input = new Scanner(System.in);
+        int userChoice = input.nextInt();
+
+        int computerChoice = (int)(Math.random() *3) + 1;
+
+        System.out.println(userChoice);
+
+        if (userChoice == 1){
+            System.out.println("User chooses" + userChoice);
+        }
+
+        if (userChoice == computerChoice) {
+            System.out.println("It's a tie!");
+        }
+
+        else if (userChoice == ROCK && computerChoice == SCISSORS) {
+            System.out.println("You win!");
+        }
     }
 
-    }    
-
 }
+
+
